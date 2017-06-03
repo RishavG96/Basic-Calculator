854fb4cfacd2967c63d66561875eea24eb8fa443
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-06-03 16:34:33 +0530

    Calculator app with input from keyboard

---
+++ app/src/main/java/rcpl/com/stp17firstapp/MainActivity.java
@@ -0,0 +1,93 @@
+package rcpl.com.stp17firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1;
+    EditText et2;
+    TextView t;
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=(EditText) findViewById(R.id.editText);
+        et2=(EditText) findViewById(R.id.editText2);
+        t=(TextView)findViewById((R.id.textView));
+        b1=(Button) findViewById(R.id.button);
+        b2=(Button) findViewById(R.id.button2);
+        b3=(Button) findViewById(R.id.button3);
+        b4=(Button) findViewById(R.id.button4);
+        b5=(Button) findViewById(R.id.button5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num1=et1.getText().toString();
+                String num2=et2.getText().toString();
+                int n1=Integer.parseInt(num1);
+                int n2=Integer.parseInt(num2);
+                int sum=n1+n2;
+                t.setText("The sum is :" +sum);
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num1=et1.getText().toString();
+                String num2=et2.getText().toString();
+                int n1=Integer.parseInt(num1);
+                int n2=Integer.parseInt(num2);
+                int sub=n1-n2;
+                t.setText("The diff is :" +sub);
+
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num1=et1.getText().toString();
+                String num2=et2.getText().toString();
+                int n1=Integer.parseInt(num1);
+                int n2=Integer.parseInt(num2);
+                int mul=n1*n2;
+                t.setText("The mul is :" +mul);
+
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num1=et1.getText().toString();
+                String num2=et2.getText().toString();
+                int n1=Integer.parseInt(num1);
+                int n2=Integer.parseInt(num2);
+                double div=n1/n2;
+                t.setText("The div is :" +div);
+
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num1=et1.getText().toString();
+                String num2=et2.getText().toString();
+                int n1=Integer.parseInt(num1);
+                int n2=Integer.parseInt(num2);
+                double mod=n1%n2;
+                t.setText("The mod is :" +mod);
+
+            }
+        });
+
+    }
+}
